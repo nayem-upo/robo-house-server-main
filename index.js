9e95c2b5b9c18d4eb88aad506b0bb8c5845ad816
nayem-upo <nayemislam0181@gmail.com>
2023-05-21 00:04:51 +0600

    post api added

--- index.js
+++ index.js
@@ -70,7 +70,12 @@ async function run() {
             res.send(result)
         })
 
-        
+        app.delete("/toy/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await toyCollection.deleteOne(query)
+            res.send(result)
+        })
 
         app.get('/toy/:id', async (req, res) => {
             const id = req.params.id;
